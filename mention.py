f0d71fc51f72dc8d8d5137f2374dab76cef392c7
Alex Ling <hkalexling@gmail.com>
2015-10-28 13:56:23 +0800

    Use streaming API to increase reply speed

--- mention.py
+++ mention.py
@@ -1,65 +1,14 @@
 #coding:utf-8
 from twython import Twython
+from twython import TwythonStreamer
 from simsimi import chat
 import sys  
 import threading
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
-time = 65
 
 twitter = Twython('Consumer Key', 'Consumer Secret', 'Access Token', 'Access Token Secret')
-
-def mention():
-	getMention()
-	threading.Timer(time, mention).start()
-
-def getMention():
-	for i in twitter.get_mentions_timeline():
-		shouldReply = False
-		tweet_id = ''
-		user = 'whoever'
-		text = ''
-
-		k = i.keys()
-		v = i.values()
-		for n in range(0, len(k)):
-			if k[n] == 'user':
-				user = handleUser(v[n])
-			
-			if k[n] == 'id':
-				tweet_id = v[n]
-			
-			if k[n] == 'in_reply_to_status_id_str':
-				repliedFile = open('replied_id.txt', 'rb')
-				if str(tweet_id) in repliedFile.read():
-					return
-				shouldReply = handleStatus(v[n])
-
-			if k[n] == 'text':
-				text = v[n].replace(v[n].split(' ')[0], '')
-			
-		if shouldReply:
-			if user != 'whoever':
-				string = '#KonaBot自动回复 ' + user + chat(text)
-				twitter.update_status(status=string, in_reply_to_status_id = tweet_id)
-				print str(string)
-				with open('replied_id.txt', 'wb') as repliedFile:
-					repliedFile.write(str(tweet_id))
-			print 'replied'
-
-		return
-	print 'finished'
-
-			
-def handleUser(input):
-	k = input.keys()
-	v = input.values()
-	for n in range(0, len(k)):
-		if k[n] == 'screen_name':
-			return ' @' + v[n] + ' '
-
-	return 'whoever'
 
 def handleStatus(input_id):
 	dict = twitter.show_status(id = input_id)
@@ -69,9 +18,26 @@ def handleStatus(input_id):
 	for n in range(0, len(k)):
 		if k[n] == 'text':
 			if '#KonaBot' in v[n]:
-				print v[n]
 				return True
 
 	return False
 
-mention()
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+    	if 'in_reply_to_status_id_str' in data:
+    		if data['in_reply_to_status_id_str'] != None:
+    			if handleStatus(data['in_reply_to_status_id_str']):
+    				tweet_id = data['id']
+    				if 'user' in data:
+    					if data['user']['screen_name'] != 'hkalexling':
+	    					user = '@' + data['user']['screen_name'] + ' '
+	    					text = data['text'].replace(data['text'].split(' ')[0], '')
+		    				string = '#KonaBot自动回复 ' + user + chat(text)
+		    				print str(string)
+		    				twitter.update_status(status=string, in_reply_to_status_id = tweet_id)
+    def on_error(self, status_code, data):
+        print status_code, data
+
+stream = MyStreamer('Consumer Key', 'Consumer Secret', 'Access Token', 'Access Token Secret')
+
+stream.user()
