ab827e87effd335b761a207e26470bec5f30eb84
Alex Ling <hkalexling@gmail.com>
2015-10-28 19:04:38 +0800

    Use konachan.net instead of konachan.com as image source to prevent sexual or offensive images

--- img.py
+++ img.py
@@ -8,7 +8,8 @@ def generateNum():
 def downloadImg():
 	rawurl = None
 	while rawurl is None: 
-	 	page = requests.get('https://konachan.com/post/show/' + generateNum())
+	 	page = requests.get('https://konachan.net/post/show/' + generateNum())
+	 	#Use 'https://konachan.com if you don't mind the bot posting some sexual or offensive images.
 		html = page.content
 		parsed = BeautifulSoup(html)
 		rawurl = parsed.body.find('div', attrs={'id':'note-container'})
