ba36ecef3670b13521c9f1f238e43c7eaad06cfd
Alex Ling <hkalexling@gmail.com>
2015-10-29 16:47:41 +0800

    Auto favorite tweets by certain users

---
+++ favorite.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+from twython import Twython
+from twython import TwythonStreamer
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+twitter = Twython('Consumer Key', 'Consumer Secret', 'Access Token', 'Access Token Secret')
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+    	if 'user' in data:
+    		userName = data['user']['screen_name']
+    		favoriteList = open('favoriteList.txt').read().split(';')
+    		if userName in favoriteList:
+    			id = data['id']
+    			twitter.create_favorite(id = id)
+    	
+    def on_error(self, status_code, data):
+        print status_code, data
+
+stream = MyStreamer('Consumer Key', 'Consumer Secret', 'Access Token', 'Access Token Secret')
+
+stream.user()
